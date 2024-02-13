b85a3a7abde30ec93df0ba98084367fd92efe3c6
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-02-13 18:49:25 -0300

    feat: Estruturando, estilizando e criando lógica do Header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
-function teste (){
-    return 2+3
+document.addEventListener('DOMContentLoaded', function () {
+const mainElement = document.querySelector('main')
+
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+        if (posicaoAtual < 1) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+})
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    const mainElement = document.querySelector('main')
+    header.classList.add('header--is-hidden');
+    mainElement.classList.remove('dark-hover');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    const mainElement = document.querySelector('main')
+    header.classList.remove('header--is-hidden');
+    mainElement.classList.add('dark-hover');
 }
